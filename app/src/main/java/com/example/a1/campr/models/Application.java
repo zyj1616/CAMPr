1c8881d0317b79513cbaf5fb5d2fcbd721d227c4
zyj1616 <y1zhou@ucsd.edu>
2018-12-01 11:51:43 -0800

    123

---
+++ app/src/main/java/com/example/a1/campr/models/Application.java
@@ -0,0 +1,61 @@
+package com.example.a1.campr.models;
+
+public class Application {
+    private String applicationId;
+    private String listerId;
+    private String adopterId;
+    private String petId;
+    private boolean approval;
+
+    public boolean isCurrentContact() {
+        return currentContact;
+    }
+
+    public void setCurrentContact(boolean currentContact) {
+        this.currentContact = currentContact;
+    }
+
+    private boolean currentContact;
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public String getListerId() {
+        return listerId;
+    }
+
+    public void setListerId(String listerId) {
+        this.listerId = listerId;
+    }
+
+    public String getAdopterId() {
+        return adopterId;
+    }
+
+    public void setAdopterId(String adopterId) {
+        this.adopterId = adopterId;
+    }
+
+    public String getPetId() {
+        return petId;
+    }
+
+    public void setPetId(String petId) {
+        this.petId = petId;
+    }
+
+    public boolean isApproval() {
+        return approval;
+    }
+
+    public void setApproval(boolean approval) {
+        this.approval = approval;
+    }
+
+
+}
